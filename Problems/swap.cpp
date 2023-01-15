e4d548d0f4199085da268022f60b1d95795bd63e
Nur Farah <nuurfaarax15@gmail.com>
2023-01-15 14:24:22 +0300

    promlems repo added

---
+++ Problems/swap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//
+// Created by abdinasir abdiasis on 1/15/2023.
+//
+int main(){
+    int a = 3;
+    int b = 5;
+    int temp = a;
+    a = b;
+    b = temp;
+    std::cout << a;
+}
+
+#include "swap.h"
---
+++ Problems/swap.h
@@ -0,0 +1,14 @@
+//
+// Created by abdinasir abdiasis on 1/15/2023.
+//
+
+#ifndef CPP_SWAP_H
+#define CPP_SWAP_H
+
+
+class swap {
+
+};
+
+
+#endif //CPP_SWAP_H
