6956df046f92dba3256c73b443ae6550cae5c796
Nur Farah <nuurfaarax15@gmail.com>
2023-01-16 20:17:01 +0300

    changes made in swap.cpp

--- Problems/swap.cpp
+++ Problems/swap.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-//
-// Created by abdinasir abdiasis on 1/15/2023.
-//
+
 int main(){
-    int a = 3;
-    int b = 5;
+    int a = 1;
+    int b = 2;
     int temp = a;
     a = b;
-    b = temp;
-    std::cout << a;
+    b= temp;
+    std::cout <<a;
+    return 0;
 }
-
-#include "swap.h"
